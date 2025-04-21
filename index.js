b5f439d4a80d64eaaa09cfdf6087decf119a8694
Rishik Pathak <157512702+Rishik3104@users.noreply.github.com>
2025-04-21 19:55:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MONGODB CONNCECTION
+// MONGODB CONNECTION
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
