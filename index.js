604310edecf1918e0c86d88b7280d422bec83a4f
Rishik Pathak <157512702+Rishik3104@users.noreply.github.com>
2025-04-21 19:55:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 // MONGODB CONNECTION
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
